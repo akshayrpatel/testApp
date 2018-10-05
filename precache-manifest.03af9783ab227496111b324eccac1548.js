f85762e2a7e88a6af86d363a4f882da318cb3773
Akshay Patel <akshay24@my.yorku.ca>
2018-10-05 10:45:36 -0400

    Updates

---
+++ precache-manifest.03af9783ab227496111b324eccac1548.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testApp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "463b738604eec5856e71",
+    "url": "/testApp/static/js/runtime~main.463b7386.js"
+  },
+  {
+    "revision": "81745f46ce62723c5105",
+    "url": "/testApp/static/js/main.81745f46.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/testApp/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "81745f46ce62723c5105",
+    "url": "/testApp/static/css/main.3fc04462.chunk.css"
+  },
+  {
+    "revision": "dc7f16f5d684ebbc2dbe4780fc4cf30b",
+    "url": "/testApp/index.html"
+  }
+];
